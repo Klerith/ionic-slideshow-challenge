53f9991e0aa01e5559f1fa7795978c3257760286
Fernando Herrera <fernando.herrera85@gmail.com>
2019-03-21 10:45:38 -0600

    primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  slides = [
+    {
+      img: 'assets/img/mujer.svg',
+      titulo: 'Visible Changes<br>in 3 weeks'
+    },
+    {
+      img: 'assets/img/pastel.svg',
+      titulo: 'Forget about<br>strict diet'
+    },
+    {
+      img: 'assets/img/dinero.svg',
+      titulo: 'Save money on<br>gym membership'
+    }
+  ];
+
+
+  constructor() {
+
+  }
+
 }
+
